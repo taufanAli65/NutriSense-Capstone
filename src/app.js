7ff95b3f112f9483358673062ef7d3102544c24b
taufanAli65 <taufanali65@gmail.com>
2024-11-30 17:33:05 +0700

    chore: rename endpoint /user to /auth

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/nutrition", nutritionRouter);
-app.use("/user", userRouter);
+app.use("/auth", userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
