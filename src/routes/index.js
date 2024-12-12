e356be2825065d2f95a672e41627de01ca061bcb
taufanAli65 <taufanali65@gmail.com>
2024-12-12 13:12:11 +0700

    feat: add route to fetch data by user ID and date with error handling

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
-const { getData } = require("../database/getData");
+const { getData, getDataByID } = require("../database/getData");
 const { authenticateToken } = require("../middleware/auth");
+const { getUserID } = require("../database/auth");
 
 /* GET home page. */
 router.get("/", authenticateToken, async (req, res) => {
@@ -16,5 +17,16 @@ router.get("/", authenticateToken, async (req, res) => {
     res.status(500).json({ message: "Internal Server Error", error: error.message });
   }
 });
+
+router.get("/sort/:date", authenticateToken, async (req, res) => {
+  try {
+    const userID = await getUserID(req);
+    const date = req.params.date;
+    const data = await getDataByID(userID, date);
+    res.status(200).json({message: "Data Fetched Successfuly", data: data});
+  } catch (error) {
+    res.status(500).json({message: "Internal Server Error", error: error.message});
+  }
+})
 
 module.exports = router;
