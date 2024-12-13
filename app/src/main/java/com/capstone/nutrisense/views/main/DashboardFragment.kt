ad3a8feca65b22dd6cecac688941b6f331cbc4ea
moehoemar <umarhatta.official@gmail.com>
2024-12-13 08:09:00 +0700

    navigation bar cant move

--- app/src/main/java/com/capstone/nutrisense/views/main/DashboardFragment.kt
+++ app/src/main/java/com/capstone/nutrisense/views/main/DashboardFragment.kt
@@ -1,9 +1,10 @@
 package com.capstone.nutrisense.views.main
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.capstone.nutrisense.R
 
-
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
